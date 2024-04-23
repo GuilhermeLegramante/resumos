354f5cb2d5c824d3894a6294ffc3d685cbe38ba5
GuilhermeLegramante <guilhermelegramante@gmail.com>
2024-04-23 17:04:39 -0300

    .

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -54,7 +54,7 @@ class AdminPanelProvider extends PanelProvider
             ->brandName('Resumos da Tina')
             ->sidebarCollapsibleOnDesktop()
             ->brandLogo(asset('img/logo.png'))
-            ->brandLogoHeight(fn () => auth()->check() ? '6rem' : '10rem')
+            ->brandLogoHeight(fn () => auth()->check() ? '6rem' : '8rem')
             ->favicon(asset('img/logo.png'))
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
